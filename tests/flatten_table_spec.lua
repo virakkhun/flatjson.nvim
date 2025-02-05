37956d9758b8f9a24dade704cef8ef4469f6955b
virakkhun <virak.dev.2000@gmail.com>
2025-02-06 00:13:30 +0700

    chore: add test, ci

---
+++ tests/flatten_table_spec.lua
@@ -0,0 +1,43 @@
+local f = require("core.flatten_table")
+local eq = assert.are.same
+
+local t = {
+	level1 = {
+		level2 = {
+			level3 = {
+				level4 = "hi just here",
+				level4a = {
+					theend = "just ended",
+				},
+			},
+		},
+	},
+}
+
+describe("flatten_table", function()
+	it("it should flat table with prefix .", function()
+		local flatted = f.flat(t, ".")
+		eq(flatted["level1.level2.level3.level4"], "hi just here")
+		eq(flatted["level1.level2.level3.level4a.theend"], "just ended")
+	end)
+
+	it("it should flat table with prefix __", function()
+		local flatted = f.flat(t, "__")
+		eq(flatted["level1__level2__level3__level4"], "hi just here")
+		eq(flatted["level1__level2__level3__level4a__theend"], "just ended")
+	end)
+
+	it(
+		"it should return expected table of { ['level1__level2__level3__level4'] = 'hi just here', ['level1__level2__level3__level4a__theend'] = 'just ended' }",
+		function()
+			local flatted = f.flat(t, "__")
+			eq(
+				flatted,
+				{
+					["level1__level2__level3__level4"] = "hi just here",
+					["level1__level2__level3__level4a__theend"] = "just ended",
+				}
+			)
+		end
+	)
+end)
